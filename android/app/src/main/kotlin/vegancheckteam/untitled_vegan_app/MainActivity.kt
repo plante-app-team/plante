d33ae9ea6ed3425682d09e385d984e8039785490
Danil Garmanov <danil.garmanov@protonmail.com>
2021-03-13 22:47:37 +0300

    Fix Android release build

--- android/app/src/main/kotlin/vegancheckteam/untitled_vegan_app/MainActivity.kt
+++ android/app/src/main/kotlin/vegancheckteam/untitled_vegan_app/MainActivity.kt
@@ -1,6 +1,11 @@
 package vegancheckteam.untitled_vegan_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
