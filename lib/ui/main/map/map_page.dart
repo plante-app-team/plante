014061b67a12e3f63255d5eaf243f60830b574b1
blazern <danil.garmanov@protonmail.com>
2021-05-27 19:15:09 +0300

    Cluster shops on the map
    https://trello.com/c/n4lgZXRH/

--- lib/ui/main/map/map_page.dart
+++ lib/ui/main/map/map_page.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:plante/model/location_controller.dart';
 import 'package:plante/l10n/strings.dart';
+import 'package:plante/model/shop.dart';
 import 'package:plante/outside/map/shops_manager.dart';
 import 'package:plante/ui/base/ui_utils.dart';
 
@@ -20,11 +23,16 @@ class _MapPageState extends State<MapPage> {
   final _shopsManager = GetIt.I.get<ShopsManager>();
   final _mapController = Completer<GoogleMapController>();
   var _initialRealPositionSet = false;
-  var _supermarketsMarkers = <Marker>{};
+  var _shopsMarkers = <Marker>{};
+  late final ClusterManager _clusterManager;
 
   @override
   void initState() {
     super.initState();
+    _clusterManager = ClusterManager<Shop>(
+        [],
+        _updateMarkers,
+        markerBuilder: _markerBuilder);
     _init();
   }
 
@@ -83,9 +91,13 @@ class _MapPageState extends State<MapPage> {
         minMaxZoomPreference: const MinMaxZoomPreference(13, 19),
         mapType: MapType.normal,
         initialCameraPosition: _initialCameraPosition(),
-        onMapCreated: _mapController.complete,
-        onCameraIdle: _obtainSupermarketsMarkers,
-        markers: _supermarketsMarkers,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+          _clusterManager.setMapController(controller);
+        },
+        onCameraMove: _clusterManager.onCameraMove,
+        onCameraIdle: _obtainShopsMarkers,
+        markers: _shopsMarkers,
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _showUser,
@@ -114,7 +126,7 @@ class _MapPageState extends State<MapPage> {
         target: LatLng(position.latitude, position.longitude), zoom: 15));
   }
 
-  void _obtainSupermarketsMarkers() async {
+  void _obtainShopsMarkers() async {
     final mapController = await _mapController.future;
     final viewBounds = await mapController.getVisibleRegion();
 
@@ -128,29 +140,76 @@ class _MapPageState extends State<MapPage> {
         showSnackBar(context.strings.global_something_went_wrong, context);
       }
       setState(() {
-        _supermarketsMarkers = {};
+        _shopsMarkers = {};
       });
       return;
     }
-    final shops =
-        shopsResult.unwrap().where((element) => element.productsCount > 0);
+    final shops = shopsResult.unwrap();
 
-    final newMarkers = <Marker>{};
-    for (final shop in shops) {
-      newMarkers.add(Marker(
-        markerId: MarkerId(shop.osmId),
-        position: LatLng(shop.latitude, shop.longitude),
-        infoWindow: InfoWindow(
-            title: shop.name,
-            snippet:
-                shop.type ?? context.strings.map_page_default_shop_type_name),
-      ));
+    _clusterManager.setItems(shops.map((shop) =>
+        ClusterItem(LatLng(shop.latitude, shop.longitude), item: shop))
+        .toList());
+    _clusterManager.updateMap();
+  }
+
+  void _updateMarkers(Set<Marker> markers) {
+    setState(() {
+      _shopsMarkers = markers;
+    });
+  }
+
+
+  Future<Marker> Function(Cluster<Shop>) get _markerBuilder =>
+          (cluster) async {
+        return Marker(
+          markerId: MarkerId(cluster.getId()),
+          position: cluster.location,
+          onTap: () {
+            print('---- $cluster');
+            cluster.items.forEach(print);
+          },
+          icon: await _getMarkerBitmap(cluster.items
+                  .where((element) => element != null)
+                  .map((e) => e!)),
+        );
+      };
+
+  Future<BitmapDescriptor> _getMarkerBitmap(Iterable<Shop> shops) async {
+    const size = 125;
+    final PictureRecorder pictureRecorder = PictureRecorder();
+    final Canvas canvas = Canvas(pictureRecorder);
+    final Color paint1Color;
+    if (shops.any((e) => e.productsCount > 0)) {
+      paint1Color = Colors.orange;
+    } else {
+      paint1Color = Colors.grey;
+    }
+    final Paint paint1 = Paint()..color = paint1Color;
+    final Paint paint2 = Paint()..color = Colors.white;
+
+    canvas.drawCircle(const Offset(size / 2, size / 2), size / 2.0, paint1);
+    canvas.drawCircle(const Offset(size / 2, size / 2), size / 2.2, paint2);
+    canvas.drawCircle(const Offset(size / 2, size / 2), size / 2.8, paint1);
+
+    if (shops.length > 1) {
+      final painter = TextPainter(textDirection: TextDirection.ltr);
+      painter.text = TextSpan(
+        text: shops.length.toString(),
+        style: const TextStyle(
+            fontSize: size / 3,
+            color: Colors.white,
+            fontWeight: FontWeight.normal),
+      );
+      painter.layout();
+      painter.paint(
+        canvas,
+        Offset(size / 2 - painter.width / 2, size / 2 - painter.height / 2),
+      );
     }
 
-    if (_supermarketsMarkers != newMarkers) {
-      setState(() {
-        _supermarketsMarkers = newMarkers;
-      });
-    }
+    final img = await pictureRecorder.endRecording().toImage(size, size);
+    final data = await img.toByteData(format: ImageByteFormat.png);
+
+    return BitmapDescriptor.fromBytes(data!.buffer.asUint8List());
   }
 }
