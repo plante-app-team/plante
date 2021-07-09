503773dd716913c14a13d438ff720921ab8c21ed
Johan Vandingenen <vandingenen.johan@gmail.com>
2021-07-09 10:51:11 +0200

    fix persmission ios

--- lib/ui/base/ui_permissions_utils.dart
+++ lib/ui/base/ui_permissions_utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plante/base/permissions_manager.dart';
 import 'package:plante/l10n/strings.dart';
 import 'package:plante/ui/base/ui_utils.dart';
+import 'dart:io';
 
 /// Returns true if permission is obtained, false otherwise.
 Future<bool> maybeRequestPermission(
@@ -16,6 +17,19 @@ Future<bool> maybeRequestPermission(
 
   if (permission == PermissionState.denied) {
     permission = await permissionsManager.request(permissionKind);
+    if (permission == PermissionState.permanentlyDenied && Platform.isIOS) {
+      // On iOS we want to avoid prompting user to go to the settings
+      // at all costs - the app won't pass the app review otherwise.
+      //
+      // On Android the situation is different - permissionsManager.status()
+      // NEVER returns 'permanentlyDenied', even if the permission is truly
+      // permanently denied by the user. The app has to request the permission
+      // again to know it's permanently denied, and the app has no way of
+      // knowing whether OS has shown the user the system permission grant-deny
+      // dialog or not.
+      return false;
+    }
+
     if (permission != PermissionState.permanentlyDenied) {
       return permission == PermissionState.granted ||
           permission == PermissionState.limited;
