1b7ab6b4b7c001ff6398a6aa17301ebf3e2ce693
blazern <danil.garmanov@protonmail.com>
2021-11-20 12:27:39 +0300

    Tiny fix

--- lib/outside/off/off_user.dart
+++ lib/outside/off/off_user.dart
@@ -1,6 +1,6 @@
 /// Fields are intentionally invalid because all requests to OFF
 /// are proxied by the Plante server anyways.
 class OffUser {
-  static const USERNAME = 'intentionally invalid';
+  static const USERNAME = 'planteuser';
   static const PASSWORD = 'intentionally invalid';
 }
