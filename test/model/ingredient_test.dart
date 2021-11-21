fc441bd3585d446d6482d9a643e5082c1464b05a
blazern <danil.garmanov@protonmail.com>
2021-11-22 00:17:40 +0300

    Formating fixes

--- test/model/ingredient_test.dart
+++ test/model/ingredient_test.dart
@@ -4,26 +4,20 @@ import 'package:test/test.dart';
 void main() {
   setUp(() {});
   test('name with language preposition en', () async {
-    final ingredient = Ingredient((e) => e
-      ..name='en:gluten'
-    );
-    expect(ingredient.name,'en:gluten');
-    expect(ingredient.cleanName(),'gluten');
+    final ingredient = Ingredient((e) => e..name = 'en:gluten');
+    expect(ingredient.name, 'en:gluten');
+    expect(ingredient.cleanName(), 'gluten');
   });
 
   test('name with language no preposition NL', () async {
-    final ingredient = Ingredient((e) => e
-      ..name='NL:gluten'
-    );
-    expect(ingredient.name,'NL:gluten');
-    expect(ingredient.cleanName(),'gluten');
+    final ingredient = Ingredient((e) => e..name = 'NL:gluten');
+    expect(ingredient.name, 'NL:gluten');
+    expect(ingredient.cleanName(), 'gluten');
   });
 
   test('name with language no preposition', () async {
-    final ingredient = Ingredient((e) => e
-      ..name='gluten'
-    );
-    expect(ingredient.name,'gluten');
-    expect(ingredient.cleanName(),'gluten');
+    final ingredient = Ingredient((e) => e..name = 'gluten');
+    expect(ingredient.name, 'gluten');
+    expect(ingredient.cleanName(), 'gluten');
   });
 }
