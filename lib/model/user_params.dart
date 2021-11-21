8310d402ac40652ee833adc6c695f73238565056
blazern <danil.garmanov@protonmail.com>
2021-11-22 00:47:18 +0300

    Tiny fix

--- lib/model/user_params.dart
+++ lib/model/user_params.dart
@@ -1,7 +1,7 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
-import 'package:plante/base/build_value_helper.dart';
 import 'package:built_value/serializer.dart';
+import 'package:plante/base/build_value_helper.dart';
 
 part 'user_params.g.dart';
 
