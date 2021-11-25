32a7023e10dc088dd09109d169c6b13476f30522
blazern <danil.garmanov@protonmail.com>
2021-11-25 17:08:12 +0300

    OFF HTTP interception change

--- lib/outside/off/off_http_interceptor.dart
+++ lib/outside/off/off_http_interceptor.dart
@@ -8,21 +8,28 @@ class OffHttpInterceptor implements off.HttpInterceptor {
   OffHttpInterceptor(this._backend);
 
   @override
-  Future<http.Request?> interceptGet(Uri uri) async {
+  Future<http.Response>? fullyInterceptGet(Uri uri) {
     // GET requests are not intercepted - Open Food Facts
     // doesn't require them to be authed.
     return null;
   }
 
   @override
-  Future<http.Request?> interceptPost(Uri uri) async {
+  Future<http.Request>? interceptGet(Uri uri) {
+    // GET requests are not intercepted - Open Food Facts
+    // doesn't require them to be authed.
+    return null;
+  }
+
+  @override
+  Future<http.Request>? interceptPost(Uri uri) async {
     return await _backend.customRequest(
         'off_proxy_form_post${uri.path}', (uri) => http.Request('POST', uri),
         queryParams: uri.queryParametersAll, headers: null);
   }
 
   @override
-  Future<http.MultipartRequest?> interceptMultipart(
+  Future<http.MultipartRequest>? interceptMultipart(
       String method, Uri uri) async {
     return await _backend.customRequest('off_proxy_multipart${uri.path}',
         (uri) => http.MultipartRequest(method, uri),
